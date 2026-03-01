fbfa12e3a89df937736f3ac8a5e915ccd08c4699
Muhammad-Faizan79 <faizan95014@gmail.com>
2026-03-01 23:07:43 +0500

    add image folder

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ var nav = document.querySelector(".navbar")
 // console.log(nav)
 function hideMenu(){
     nav.style.left ="380px";
+    nav.style.display = "none";
     // console.log("hello1")
 }
 function showMenu(){
     nav.style.left ="180px";
+    nav.style.display = "block";
     // console.log("hello5" )
 }
