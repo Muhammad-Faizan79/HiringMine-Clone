40cd9c0bbcb2ee3ec6a97525c6c89f01a5f08651
Muhammad-Faizan79 <faizan95014@gmail.com>
2026-03-01 22:38:59 +0500

    make responsive

---
+++ app.js
@@ -0,0 +1,10 @@
+var nav = document.querySelector(".navbar")
+// console.log(nav)
+function hideMenu(){
+    nav.style.left ="380px";
+    // console.log("hello1")
+}
+function showMenu(){
+    nav.style.left ="180px";
+    // console.log("hello5" )
+}
